f2febbd37df86e619cc4d653732de494fd291732
Guru e <guru@Gurus-MBP.attlocal.net>
2024-07-28 00:06:22 -0500

    added additional packages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,10 +8,10 @@ public class Main {
     public static void main(String[] args) {
         /* Single Dimensional Array Operations */
         SourceArray sourceArray = new SourceArray();
-        int [] singleDim = new int[100];
+        int[] singleDim = new int[100];
         singleDim = sourceArray.singleDimensionalArray(singleDim,100000);
         List<Integer> singleDimList = Arrays.stream(singleDim).boxed().toList();
-        System.out.println("singleDim="+singleDim+" and singleDimList="+singleDimList);
+        System.out.println("singleDim="+Arrays.toString(singleDim) +"\nsingleDimList="+singleDimList);
 
         int [][] twoDim = new int [][] {{0,0},{1,1}};
         int [][][] threeDim = new int [][][]{{{0,0},{1,1}},{{0,0},{1,1}}};
