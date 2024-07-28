9563f464d74a5c97bc8828593a6dce3c2f26712d
Guru e <guru@Gurus-MBP.attlocal.net>
2024-07-27 23:58:55 -0500

    added package informaion

---
+++ src/main/java/org/example/designpatterns/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Design Patterns Exploration
+ * https://www.digitalocean.com/community/tutorials/java-design-patterns-example-tutorial#miscellaneous-design-patterns
+ */
+package org.example.designpatterns;
